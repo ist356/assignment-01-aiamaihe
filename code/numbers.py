6a2581311e1c4d71f21f25b680d877a82d9a88f7
aiamaihe <119700652+aiamaihe@users.noreply.github.com>
2024-11-04 13:38:09 -0500

    completed assignment and fixed numbers.py all tests passed

--- code/numbers.py
+++ code/numbers.py
@@ -12,3 +12,21 @@ The output should be:
 {'odd': [3, 5], 'even': [2, 4, 6]}
 '''
 
+def order_numbers():
+    numbers = {'odd': [], 'even': []}
+
+    while True:
+        try:
+            num = int(input("Enter an integer (0 to stop): "))
+            if num == 0:
+                break
+            elif num % 2 == 0:
+                numbers['even'].append(num)
+            else:
+                numbers['odd'].append(num)
+        except ValueError:
+            print("Please enter a valid integer.")
+    
+    print("Ordered numbers:", numbers)
+
+order_numbers()
